fa9dbdcf3fa2b4c427f95f3f153390bc92d7680e
Run Qi (Jack) Li <li.jack0707@gmail.com>
2024-03-17 18:11:26 -0400

    WIP - create Nodes, linked list, append function

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿public class Node
+{
+    public int data;
+    public Node? next;
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList
+{
+    public Node? head; 
+    public Node? last;
+    public Node? current;
+
+    public LinkedList()
+    {
+        this.head = null;
+    }
+
+    public void Append(int data)
+    {
+        Node newNode = new Node(data);
+
+        if(head is null) head = newNode;
+
+        else
+        {
+            Node current = head;
+            while (current.next != null) current = current.next;
+            current.next = newNode;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node? current = head;
+        while (current != null)
+        {
+            Console.WriteLine(current.data);
+            current = current.next;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        LinkedList linkedList = new LinkedList();
+        linkedList.Append(1);
+        linkedList.Append(2);
+        linkedList.Append(3);
+        linkedList.PrintList();
+    }
+
+}
