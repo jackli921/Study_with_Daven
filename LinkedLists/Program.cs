b2b5ba1693e2b8e45a57b5e9c43396ff2f4e3aba
Run Qi (Jack) Li <li.jack0707@gmail.com>
2024-03-19 22:09:37 -0400

    first attempt at implementing delete at index

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -69,7 +69,7 @@ public class Program
         link_list.Append(2);
         link_list.Append(3);
         link_list.PrintList();
-        link_list.Insert(4, 1);
+        link_list.Delete(0);
         link_list.PrintList();
     } 
 }
@@ -114,15 +114,6 @@ public class LinkedList
         }
     }
 
-    //inserting a node at a specific position
-
-    // need to check if index is out of range or smaller than 0
-
-    // need to check if head is null and index is 0, we make new node the head
-
-    // traverse the existing linklist to one before the index
-    // connect the tail of the current list to new_node.next
-    // connect current.next to new_node
     public void Insert(int data, int index)
     {
         // if index is smaller than 0
@@ -167,6 +158,65 @@ public class LinkedList
 
         new_node.next = current.next;
         current.next = new_node;
+    }
+
+    // delete at index
+    public void Delete(int index)
+    {
+        // if index is smaller than 0
+        if(index < 0)
+        {
+            System.Console.WriteLine("index out of range");
+            return;
+        }
+        
+        if (head == null)
+        {
+            System.Console.WriteLine("Empty list, nothing to delete");
+            return;
+        }
+
+        // if index is 0, we use current.next as the new head
+            // if there is 1 or  more than 1 node in the remaining list
+            // if the link list was size of 1 and there is nothing left after removing first
+
+        if (index == 0)
+        {
+            // start at index 0
+            this.current = this.head;
+            
+            // if there's nothing after the first node, then empty the link
+            if (this.current.next == null)
+            {
+                this.head = null;
+                return;
+            }
+            
+            //if there are nodes after the first, make the next immediate node the hed 
+            this.head = this.current.next;
+            return;
+        }
+        
+        // last scenario: removing a node between nodes
+        // if the position being removed is the last one, then there's nothing to attach 
+
+        // else, attach current.next with the node after the one at our index 
+
+        this.current = this.head;
+        int count = 0; 
+        while (this.current.next != null && count < index - 1)
+        {
+            this.current = this.current.next;
+            count++;
+        }
+
+        if (count != index -1 || current == null || current.next == null)
+        {
+            System.Console.WriteLine("index out of range");
+            return;
+        }
+
+        this.current.next = this.current.next.next;
     }
 
     public void PrintList()
